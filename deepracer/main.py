100dcad7f25cf5264c6a3fdbcc8a6adebdacd8b4
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2021-03-08 15:08:42 +0800

    Add logging to Flask App

--- deepracer/main.py
+++ deepracer/main.py
@@ -2,21 +2,27 @@ import flask
 from flask import request, jsonify
 import json
 from core.web_core import Client
+from flask_web_log import Log
+import logging
 
 app = flask.Flask(__name__)
+app.config["LOG_TYPE"] = "STDOUT"
 app.config["DEBUG"] = True
+app.logger.setLevel(logging.DEBUG)
+Log(app)
 
-client = Client(password="39282662", ip="172.0.0.1")
+client = Client(password="mBjaEwrN", ip="localhost")
 
 @app.route('/', methods=['GET'])
 def api():   
     # data = json.load(request.data.decode('utf-8'))
-    print(request.headers)
-    print(request.json)
-    # try:
-    #     client.start_car()
-    # except Exception as err:
-    #     print(err)
+    app.logger.info(request.headers)
+    app.logger.info(request.json)
+    try: 
+        app.logger.info("Call DeepRacer")       
+        client.start_car()
+    except Exception as err:
+        app.logger.error(err)       
     return "ok"
 
 app.run()
--- gamepad/main.py
+++ gamepad/main.py
@@ -2,3 +2,4 @@ from controller import Client
 
 client = Client(password="39282662", ip="127.0.0.1:8080")
 client.start_car()
+# client.stop_car()
