6d3274fba4c616b2cb667d9da5af1c7d021e4d04
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2021-03-09 13:41:31 +0800

    Add controller example.

--- gamepad/main.py
+++ gamepad/main.py
@@ -1,6 +1,8 @@
 from controller import Client
+import time
 
 client = Client(password="39282662", ip="127.0.0.1:8080")
 client.start_car()
 client.move(-0.46923076923076923,-0.9769230769230769,0.5)
+time.sleep(5)
 client.stop_car()
